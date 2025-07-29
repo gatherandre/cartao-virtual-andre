7a2ee6d1d30c6022f30375bc3765c66c4f5c222d
gatherandre <andregimaranz@gmail.com>
2025-07-29 18:58:11 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,50 @@
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('card-cache').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json',
-      './icon-192.png',
-      './icon-512.png',
-      './bg.jpg',
-      './qr_vcard.png',
-      './andre_guimaraes.vcf'
-    ]))
+const CACHE_NAME = 'card-cache-v2';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './bg.jpg',
+  './qr_vcard.png',
+  './andre_guimaraes.vcf'
+];
+
+// Instala e adiciona arquivos ao cache
+self.addEventListener('install', (event) => {
+  console.log('[ServiceWorker] Instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Cacheando arquivos');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Ativa e remove caches antigos
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Ativando');
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removendo cache antigo', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições e serve do cache se disponível
+self.addEventListener('fetch', (event) => {
+  console.log('[ServiceWorker] Buscando', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
