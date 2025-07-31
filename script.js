a48ab090e34c89493b75b2f062b1146b162fa807
gatherandre <andregimaranz@gmail.com>
2025-07-31 15:00:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ function mostrarDados() {
   }
 }
 
+function exportarVCF() {
+  const dados = JSON.parse(localStorage.getItem('cartaoFlamengo'));
+  if (!dados) return;
+
+  const vcf = `BEGIN:VCARD\nVERSION:3.0\nN:${dados.nome};;;;\nFN:${dados.nome}\nTITLE:${dados.cargo}\nTEL;TYPE=CELL:${dados.telefone}\nEMAIL:${dados.email}\nEND:VCARD`;
+  const blob = new Blob([vcf], { type: 'text/vcard' });
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = 'cartao.vcf';
+  a.click();
+}
+
+function gerarQRCode() {
+  const dados = JSON.parse(localStorage.getItem('cartaoFlamengo'));
+  if (!dados) return;
+
+  const vcf = `BEGIN:VCARD\nVERSION:3.0\nN:${dados.nome};;;;\nFN:${dados.nome}\nTITLE:${dados.cargo}\nTEL;TYPE=CELL:${dados.telefone}\nEMAIL:${dados.email}\nEND:VCARD`;
+
+  const qrUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' + encodeURIComponent(vcf);
+  document.getElementById('qr').innerHTML = '<img src="' + qrUrl + '" alt="QR Code">';
+}
+
 mostrarDados();
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('fla-card').then(cache => {
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
@@ -14,8 +14,10 @@ self.addEventListener('install', e => {
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
